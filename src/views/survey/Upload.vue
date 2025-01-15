fdd251bc089d1bfa2d6de2b2c4d94dfeda53efb1
Wouter van Dam <wouter@journeyworks.nl>
2025-01-15 16:25:31 +0100

    linting on upload.vue

--- src/views/survey/Upload.vue
+++ src/views/survey/Upload.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { ref, type Ref } from 'vue';
-import { storeToRefs } from 'pinia';
+import { ref, type Ref } from 'vue'
+import { storeToRefs } from 'pinia'
 
 import Title from '@/components/Title.vue'
 
@@ -11,16 +11,16 @@ const { Model } = storeToRefs(useSurveyStore())
 const loadedFiles: Ref<File[]> = ref([])
 
 /**
- * TODO: Upload API ... 
+ * TODO: Upload API ...
  * TODO: Dropzone ?
- * TODO: Allow remove files? 
+ * TODO: Allow remove files?
  * TODO: Is old implementation limited to 25 files ?
  * TODO: Styling when uploading multiple, and in particular more than 5 files...
  */
-const loadfiles = function loadFiles(e: Event) {  
+const loadfiles = function loadFiles(e: Event) {
   const target = e.target as HTMLInputElement
   if (target && target.files) {
-    console.log("upload files...")
+    console.log('upload files...')
 
     loadedFiles.value = Array.from(target.files)
 
@@ -33,26 +33,32 @@ const loadfiles = function loadFiles(e: Event) {
     // stream: file.OpenReadStream(512 * 1024 * 1024));
     // loadedFiles.Add(file);
 
-    Model.value.document_file = loadedFiles.value.map(file => file.name)
+    Model.value.document_file = loadedFiles.value.map((file) => file.name)
   }
 }
 </script>
 
 <template>
   <div class="Upload__Wrapper">
-    <Title message="Heeft u informatie beschikbaar?" subtitle="U kunt deze stap ook overslaan" :center="true" />
+    <Title
+      message="Heeft u informatie beschikbaar?"
+      subtitle="U kunt deze stap ook overslaan"
+      :center="true"
+    />
 
     <form id="upload-area" class="UploadArea dropzone dz-clickable">
       <label for="file-upload">
         <div class="dz-message align-self-center">
-          <input type="file" id="file-upload" style="display:none" @change="loadfiles" multiple />
+          <input type="file" id="file-upload" style="display: none" @change="loadfiles" multiple />
 
           <template v-if="loadedFiles.length !== 0">
-            <div style="display: flex; justify-content: center;">
+            <div style="display: flex; justify-content: center">
               <div v-for="file in loadedFiles">
-                <img alt="uploaded"
+                <img
+                  alt="uploaded"
                   src="https://images.freeimages.com/fic/images/icons/2813/flat_jewels/512/file.png"
-                  width="150">
+                  width="150"
+                />
                 <p>{{ file.name }}</p>
               </div>
             </div>
@@ -61,12 +67,12 @@ const loadfiles = function loadFiles(e: Event) {
             <img alt="upload" src="/img/upload.svg" />
           </template>
 
-          <p class="mb-0 mt-3">
+          <div class="mb-0 mt-3">
             <strong>Upload onderzoeksrapporten, archiefstukken, tekeningen of foto's</strong>
             <div>
               <span>Klik om een bestand te kiezen</span>
             </div>
-          </p>
+          </div>
         </div>
       </label>
     </form>
@@ -75,63 +81,63 @@ const loadfiles = function loadFiles(e: Event) {
 
 <style>
 .Upload__Wrapper {
-    height: 100%;
-    padding: 20px 20px;
+  height: 100%;
+  padding: 20px 20px;
 
-    max-width: 100%;
+  max-width: 100%;
 }
 
 .Upload__Wrapper .Title {
-    margin-bottom: 4px;
+  margin-bottom: 4px;
 }
 
 .Upload__Wrapper .BodyText {
-    max-width: 100%;
-    margin-bottom: 26px;
-    font-size: 16px;
+  max-width: 100%;
+  margin-bottom: 26px;
+  font-size: 16px;
 }
 
 @media only screen and (min-width: 900px) {
-    .Upload__Wrapper {
-        padding: 50px 80px;
-    }
+  .Upload__Wrapper {
+    padding: 50px 80px;
+  }
 }
 
 .Title {
-    margin-bottom: 26px;
+  margin-bottom: 26px;
 }
 
 /*  */
 
 .UploadArea {
-    width: 100%;
-    border-radius: 5px;
-    background-color: hsla(0,0%,100%,.7);
-    border: 1px solid #d4daf0;
-    user-select: none;
-    cursor: pointer;
+  width: 100%;
+  border-radius: 5px;
+  background-color: hsla(0, 0%, 100%, 0.7);
+  border: 1px solid #d4daf0;
+  user-select: none;
+  cursor: pointer;
 }
 
 .UploadArea:hover {
-    background-color: #f0f3fd;
+  background-color: #f0f3fd;
 }
 
 .dropzone .dz-message {
-    text-align: center;
-    padding: 20px 20px;
-    /* margin: 2em 0; */
+  text-align: center;
+  padding: 20px 20px;
+  /* margin: 2em 0; */
 }
 
-.dropzone.dz-clickable .dz-message, .dropzone.dz-clickable .dz-message * {
-    cursor: pointer;
+.dropzone.dz-clickable .dz-message,
+.dropzone.dz-clickable .dz-message * {
+  cursor: pointer;
 }
 
 .UploadArea p {
-    text-align: center;
-    font-size: 14px;
-    color: #354052;
-    font-weight: 300;
-    line-height: 17px;
+  text-align: center;
+  font-size: 14px;
+  color: #354052;
+  font-weight: 300;
+  line-height: 17px;
 }
-
 </style>
