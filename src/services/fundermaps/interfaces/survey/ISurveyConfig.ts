6fbfd613506c06705b3b25c210f1283c08772e86
Yorick de Wid <yorickdewid@users.noreply.github.com>
2025-04-10 12:52:29 +0200

    Remove vendor_slug from ISurveyConfig interface

--- src/services/fundermaps/interfaces/survey/ISurveyConfig.ts
+++ src/services/fundermaps/interfaces/survey/ISurveyConfig.ts
@@ -2,7 +2,6 @@ export interface ISurveyConfig {
   client_id: number
   branding: {
     vendor_name: string
-    vendor_slug: string
     primary_color: string
     secondary_color: string
     vendor_logo_path: string
